ef9e8f658a6dc42428691e6655c53585d377bbee
Gonzalo <gonzalorpg@gmail.com>
2021-01-11 20:57:41 -0300

    agregado un for para hacer el preprocessing a toda una carpeta

--- Models/VisualSearchZeroShot/PreprocessNaturalDesign.m
+++ Models/VisualSearchZeroShot/PreprocessNaturalDesign.m
@@ -8,21 +8,42 @@ close all;
 clc;
 
 
-trialname = 'gray004.jpg';
-img = imread(['sampleimg/gray004.jpg']);
-img = imresize(img, [1028 1280]);
+imagesFolder = 'Images/';
+enumeratedImages = dir([imagesFolder '*.jpg']);
+for j = 1: length(enumeratedImages)
+    trialname = enumeratedImages(j).name;
+    img = imread([imagesFolder trialname]);
+    img = imresize(img, [1028 1280]) ;
+    
+    fun = @(block_struct) imwrite(block_struct.data,['choppednaturaldesign/img_id' trialname(5:end-4) '_' num2str(block_struct.location(1)) '_' num2str(block_struct.location(2)) '.jpg']);    
+    blockproc(img,[224 224],fun);
+    
+    objfileID = fopen('croppednaturaldesign_img.txt','w');
+    
+    chopdir = dir(['choppednaturaldesign/img_id' trialname(5:end-4) '_*.jpg']);
+    for i = 1: length(chopdir)
+        fprintf(objfileID,'%s\n',['choppednaturaldesign/' chopdir(i).name]);
+    end
 
-fun = @(block_struct) imwrite(block_struct.data,['choppednaturaldesign/img_id' trialname(5:end-4) '_' num2str(block_struct.location(1)) '_' num2str(block_struct.location(2)) '.jpg']);    
-blockproc(img,[224 224],fun);
-
-objfileID = fopen('croppednaturaldesign_img.txt','w');
-  
-
-chopdir = dir(['choppednaturaldesign/img_id' trialname(5:end-4) '_*.jpg']);
-for j = 1: length(chopdir)
-    fprintf(objfileID,'%s\n',['choppednaturaldesign/' chopdir(j).name]);
+	fclose(objfileID);
 end
 
-fclose(objfileID);
 
-    
+
+%%trialname = 'gray004.jpg';
+%%img = imread(['sampleimg/gray004.jpg']);
+%%img = imresize(img, [1028 1280]);
+
+%%fun = @(block_struct) imwrite(block_struct.data,['choppednaturaldesign/img_id' trialname(5:end-4) '_' num2str(block_struct.location(1)) '_' num2str(block_struct.location(2)) '.jpg']);    
+%%blockproc(img,[224 224],fun);
+
+%%objfileID = fopen('croppednaturaldesign_img.txt','w');
+  
+
+%%chopdir = dir(['choppednaturaldesign/img_id' trialname(5:end-4) '_*.jpg']);
+%%for j = 1: length(chopdir)
+    %%fprintf(objfileID,'%s\n',['choppednaturaldesign/' chopdir(j).name]);
+%%end
+
+%%fclose(objfileID);
+
