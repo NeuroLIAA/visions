8523ba3e94db800269519a892c433b6abf17cbdf
FerminT <fermintravi@gmail.com>
2021-01-16 12:45:14 -0300

    La división en bloques ahora no depende del tamaño

--- Models/VisualSearchZeroShot/run.py
+++ Models/VisualSearchZeroShot/run.py
@@ -26,29 +26,36 @@ def preprocess_images():
         divide_into_blocks(img, imgID, choppedDir)
 
 def divide_into_blocks(image, imgID, save_path):
-    # Create blocks of size 224x224, if possible
-    row = 0
-    column = 0
-    for block_number in range(30):
-        if row < 4:
-            if column < 5:
-                block_size = (224, 224)
-            else:
-                block_size = (224, 160)
-        elif column < 5:
-            block_size = (132, 224)
-        else:
-            block_size = (132, 160)
-            
-        from_x = column * 224
-        from_y = row * 224
-        to_y = from_y + block_size[0]
-        to_x = from_x + block_size[1]
-        img_crop = image[from_y:to_y, from_x:to_x]
-        io.imsave(save_path + '/img_id' + imgID + '_' + str(block_number) + '.jpg', img_as_ubyte(img_crop), check_contrast=False)
-        column += 1
-        if column == 6:
-            column = 0
-            row += 1
+    # Create blocks of size 224 x 224
+    img_height, img_width = image.shape[0], image.shape[1]
+    block_height, block_width = 224, 224
+
+    amount_of_rows    = img_height // block_height
+    amount_of_columns = img_width // block_width
+    extra_row    = (img_height % block_height > 0)
+    extra_column = (img_width % block_width > 0)
+    if (extra_row):
+        extra_row_height = img_height % block_height
+        amount_of_rows += 1
+    if (extra_column):
+        extra_column_width = img_width % block_width
+        amount_of_columns += 1
+    
+    block_number = 1
+    for row in range(amount_of_rows):
+        block_size = (block_height, block_width)
+        if (extra_row and (row + 1) == amount_of_rows):
+            block_size = (extra_row_height, block_size[1])
+        for column in range(amount_of_columns):
+            if (extra_column and (column + 1) == amount_of_columns):
+                block_size = (block_size[0], extra_column_width)
+            from_x = column * block_width
+            from_y = row * block_height
+            to_y = from_y + block_size[0]
+            to_x = from_x + block_size[1]
+            img_crop = image[from_y:to_y, from_x:to_x]
+            io.imsave(save_path + '/img_id' + imgID + '_' + str(block_number) + '.jpg', img_as_ubyte(img_crop), check_contrast=False)
+            block_number += 1
+
 
 main()
