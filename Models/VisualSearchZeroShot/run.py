b9d19813d1ceb341642fad8ce7724682f95c830a
FerminT <fermintravi@gmail.com>
2021-01-22 15:14:24 -0300

    Refactorizacion

--- Models/VisualSearchZeroShot/run.py
+++ Models/VisualSearchZeroShot/run.py
@@ -5,61 +5,37 @@ import json
 from os import mkdir, listdir, path
 from skimage import io, color, transform, img_as_ubyte, exposure
 
+stimuliDir = 'stimuli/'
+choppedDir = 'choppednaturaldesign/'
+resultsDir = 'results/'
+stimuliSize = (1028, 1280)
 
 def main():
-    stimuliDir = 'stimuli/'
-    choppedDir = 'choppednaturaldesign/'
-    stimuliSize = (1028, 1280)
-
-    preprocess_images(stimuliDir, choppedDir, stimuliSize)
+    preprocess_images()
     run_model()
-    compute_scanpaths(stimuliDir, choppedDir, stimuliSize)
+    compute_scanpaths()
 
-def compute_scanpaths(stimuliDir, choppedDir, stimuliSize):
+def compute_scanpaths():
     enumeratedImages = listdir(stimuliDir)
-    layerList = np.array([1])
-    prejsonstructs = []
+    scanpaths = []
+
     for imageName in enumeratedImages:
         if not(imageName.endswith('.jpg')):
             continue
         
-        imgID = imageName[3:-4]
-        choppedImgDir = choppedDir + 'img' + imgID + '/'
-        choppedData = listdir(choppedImgDir)
-        template    = np.zeros([stimuliSize[0], stimuliSize[1]])
-        for layer in range(len(layerList)):
-            for choppedSaliencyData in choppedData:
-                if (choppedSaliencyData.endswith('.jpg')):
-                    continue
-
-                imgName    = choppedSaliencyData[:-17]
-                choppedImg = io.imread(choppedImgDir + imgName + '.jpg')
-                # Load data computed by the model
-                choppedSaliencyImg = scipy.io.loadmat(choppedImgDir + choppedSaliencyData)
-                choppedSaliencyImg = choppedSaliencyImg['x']
-                choppedSaliencyImg = transform.resize(choppedSaliencyImg, choppedImg.shape)
-                # Get coordinate information from the chopped image name
-                imgNameSplit = imgName.split('_')
-                from_row     = int(imgNameSplit[2]) - 1
-                from_column  = int(imgNameSplit[3]) - 1
-                # Replace in template
-                template[from_row:(from_row + choppedImg.shape[0]), from_column:(from_column + choppedImg.shape[1])] = choppedSaliencyImg
-            saliencyImg = img_as_ubyte(exposure.rescale_intensity(template))
-            # For debugging
-            io.imsave(imgID + '_saliency.jpg', saliencyImg)
+        saliencyImg = load_model_data(imageName)
 
         maxFixations  = 80
         receptiveSize = 200
+        # Target coordinates are True values in target_location
         target_location = io.imread(stimuliDir + 'gt/' + imageName)
         target_location = color.rgb2gray(transform.resize(target_location, stimuliSize))
         target_location = target_location > 0.5
-        # Target coordinates are True values in target_location
-        target_found = False
-        # Compute scanpaths from saliency image
+
         xCoordFixationOrder = []
         yCoordFixationOrder = []
-        
-
+        target_found = False
+        # Compute scanpaths from saliency image        
         for fixationNumber in range(maxFixations):
             coordinates = np.where(saliencyImg == np.amax(saliencyImg))
             posX = coordinates[0][0]
@@ -90,17 +66,49 @@ def compute_scanpaths(stimuliDir, choppedDir, stimuliSize):
         if (target_found):
             print(imageName + "; target found at fixation step " + str(fixationNumber + 1))
         # JSON encoding
-        prejsonstructs.append({ "X" : xCoordFixationOrder, "Y" : yCoordFixationOrder, "dataset" : "VisualSearchZeroShot Natural Design Dataset", "image" : imgID + ".jpg", "split" : "test", "subject" : "VisualSearchZeroShot Model" , "target" : "te la debo" })
+        scanpaths.append({ "X" : xCoordFixationOrder, "Y" : yCoordFixationOrder, "dataset" : "VisualSearchZeroShot Natural Design Dataset", "image" : imageName, "split" : "test", "subject" : "VisualSearchZeroShot Model" , "target" : "te la debo" })
     
-    jsonStructsFile = open('scanpathspython.json', 'w')
-    json.dump(prejsonstructs, jsonStructsFile, indent = 4)
+    jsonStructsFile = open(resultsDir + 'scanpathspython.json', 'w')
+    json.dump(scanpaths, jsonStructsFile, indent = 4)
     jsonStructsFile.close()
 
+def load_model_data(imageName):
+    # Get saliency map for stimuli
+    imgID = imageName[3:-4]
+    choppedImgDir = choppedDir + 'img' + imgID + '/'
+    choppedData = listdir(choppedImgDir)
+
+    template  = np.zeros([stimuliSize[0], stimuliSize[1]])
+    layerList = np.array([1])
+    for layer in range(len(layerList)):
+        for choppedSaliencyData in choppedData:
+            if (choppedSaliencyData.endswith('.jpg')):
+                continue
+
+            choppedImgName = choppedSaliencyData[:-17]
+            choppedImg     = io.imread(choppedImgDir + choppedImgName + '.jpg')
+            choppedImg_height = choppedImg.shape[0]
+            choppedImg_width  = choppedImg.shape[1]
+            # Load data computed by the model
+            choppedSaliencyImg = scipy.io.loadmat(choppedImgDir + choppedSaliencyData)
+            choppedSaliencyImg = choppedSaliencyImg['x']
+            choppedSaliencyImg = transform.resize(choppedSaliencyImg, (choppedImg_height, choppedImg_width))
+            # Get coordinate information from the chopped image name
+            choppedImgNameSplit = choppedImgName.split('_')
+            from_row    = int(choppedImgNameSplit[2])
+            from_column = int(choppedImgNameSplit[3])
+            to_row    = from_row + choppedImg_height
+            to_column = from_column + choppedImg_width
+            # Replace in template
+            template[from_row:to_row, from_column:to_column] = choppedSaliencyImg
+        saliencyImg = img_as_ubyte(exposure.rescale_intensity(template))
+    
+    return saliencyImg
 
 def run_model():
     subprocess.run("th IVSNtopdown_30_31_naturaldesign.lua", shell=True, check=True)
 
-def preprocess_images(stimuliDir, choppedDir, stimuliSize):
+def preprocess_images():
     # Preprocessing of images (conversion to grayscale, resizing and dividing into blocks)
     enumeratedImages = listdir(stimuliDir)
 
@@ -108,12 +116,15 @@ def preprocess_images(stimuliDir, choppedDir, stimuliSize):
         if not(imageName.endswith('.jpg')):
             continue
 
+        imgID = imageName[3:-4]
         img = io.imread(stimuliDir + imageName)
         if len(img.shape) >= 3:
             img = color.rgb2gray(img)
         img = transform.resize(img, stimuliSize)
 
-        imgID = imageName[3:-4]
+        if not(path.exists(choppedDir)):
+            mkdir(choppedDir)
+
         choppedImgDir = choppedDir + 'img' + imgID
         if not(path.exists(choppedImgDir)):
             mkdir(choppedImgDir)
@@ -123,31 +134,32 @@ def preprocess_images(stimuliDir, choppedDir, stimuliSize):
 def divide_into_blocks(image, imgID, save_path):
     # Create blocks of size 224 x 224
     img_height, img_width = image.shape[0], image.shape[1]
-    block_height, block_width = 224, 224
+    default_block_height, default_block_width = 224, 224
 
-    number_of_rows    = img_height // block_height
-    number_of_columns = img_width // block_width
-    extra_row    = (img_height % block_height > 0)
-    extra_column = (img_width % block_width > 0)
+    number_of_rows    = img_height // default_block_height
+    number_of_columns = img_width // default_block_width
+    extra_row    = (img_height % default_block_height > 0)
+    extra_column = (img_width % default_block_width > 0)
     if (extra_row):
-        extra_row_height = img_height % block_height
+        extra_row_height = img_height % default_block_height
         number_of_rows += 1
     if (extra_column):
-        extra_column_width = img_width % block_width
+        extra_column_width = img_width % default_block_width
         number_of_columns += 1
     
     for row in range(number_of_rows):
-        block_size = (block_height, block_width)
+        current_block_size = (default_block_height, default_block_width)
         if (extra_row and (row + 1) == number_of_rows):
-            block_size = (extra_row_height, block_size[1])
+            current_block_size = (extra_row_height, current_block_size[1])
         for column in range(number_of_columns):
             if (extra_column and (column + 1) == number_of_columns):
-                block_size = (block_size[0], extra_column_width)
-            from_x = column * block_width
-            from_y = row * block_height
-            to_y = from_y + block_size[0]
-            to_x = from_x + block_size[1]
-            img_crop = image[from_y:to_y, from_x:to_x]
-            io.imsave(save_path + '/img_id' + imgID + '_' + str(from_y + 1) + '_' + str(from_x + 1) + '.jpg', img_as_ubyte(img_crop), check_contrast=False)
+                current_block_size = (current_block_size[0], extra_column_width)
+            
+            from_row    = row * default_block_height
+            from_column = column * default_block_width
+            to_row    = from_row + current_block_size[0]
+            to_column = from_column + current_block_size[1]
+            img_crop = image[from_row:to_row, from_column:to_column]
+            io.imsave(save_path + '/img_id' + imgID + '_' + str(from_row) + '_' + str(from_column) + '.jpg', img_as_ubyte(img_crop), check_contrast=False)
 
 main()
