732afd8ed9589b8e10036e4130bb80f4b604e237
FerminT <fermintravi@gmail.com>
2021-01-25 10:19:35 -0300

    Script arreglado y datos de sujetos en JSON

--- Datasets/VisualSearchZeroShot/save_to_json.py
+++ Datasets/VisualSearchZeroShot/save_to_json.py
@@ -15,12 +15,13 @@ trialsSequence = trialsSequence % numImages
 subjectsFiles = listdir(subjectsFilesDir)
 subjectsTrialsInfo = []
 for subjectDataFile in subjectsFiles:
-    if (subjectDataFile.endswith('_oracle.mat') or subjectDataFile.endswith('.m')):
+    if (subjectDataFile.endswith('_oracle.mat') or not(subjectDataFile.endswith('.mat'))):
         continue
 
     subjectNumber = subjectDataFile[4:6]
+    print("Processing " + subjectsFilesDir + subjectDataFile)
     currentSubjectData = loadmat(subjectsFilesDir + subjectDataFile)
-    stimuliProcessed = np.empty(numImages*17)
+    stimuliProcessed = np.empty(numImages * 17)
     for trialNumber in range(len(trialsSequence)):
         stimuli = trialsSequence[trialNumber]
         if (stimuli in stimuliProcessed):
@@ -31,7 +32,10 @@ for subjectDataFile in subjectsFiles:
         fix_posY = currentSubjectData['FixData']['Fix_posy'][0][0][trialNumber][0].flatten()
         # First fixation is always at the center of the stimuli, it isn't taken into account in the number of fixations
         number_of_fixations = len(fix_posX) - 1
-        target_found = bool(currentSubjectData['FixData']['TargetFound'][0][0][trialNumber][(number_of_fixations - 1)])
+        target_found = False
+        # Target found matrix has only 74 columns
+        if (number_of_fixations < 74):
+            target_found = bool(currentSubjectData['FixData']['TargetFound'][0][0][trialNumber][(number_of_fixations - 1)])
 
         fix_startTime = currentSubjectData['FixData']['Fix_starttime'][0][0][trialNumber][0].flatten()
         fix_endTime = currentSubjectData['FixData']['Fix_time'][0][0][trialNumber][0].flatten()
@@ -45,9 +49,9 @@ for subjectDataFile in subjectsFiles:
             stimuliName = '0' + stimuliName
         imageName = 'img' + str(stimuliName) + '.jpg'
 
-        subjectsTrialsInfo.append({ "X" : fix_posX, "Y" : fix_posY, "T" : fix_time, "dataset" : "VisualSearchZeroShot Natural Design Dataset", "image" : imageName, "split" : "valid", "subject" : subjectNumber, "target object" : "te la debo", "maximum fixations" : "80", "target found"  : str(target_found) })
-        np.append(stimuliProcessed,[stimuli])
+        subjectsTrialsInfo.append({ "X" : fix_posX.tolist(), "Y" : fix_posY.tolist(), "T" : fix_time.tolist(), "dataset" : "VisualSearchZeroShot Natural Design Dataset", "image" : imageName, "split" : "valid", "subject" : subjectNumber, "target object" : "te la debo", "maximum fixations" : "80", "target found"  : str(target_found) })
+        np.append(stimuliProcessed, [stimuli])
 
 jsonStructsFile = open(subjectsFilesDir + 'human_scanpaths.json', 'w')
-json.dump(scanpaths, jsonStructsFile, indent = 4)
+json.dump(subjectsTrialsInfo, jsonStructsFile, indent = 4)
 jsonStructsFile.close()
