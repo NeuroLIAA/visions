a05e05634f4d94e539984cb5f6569e97f505a69f
FerminT <fermintravi@gmail.com>
2021-01-27 16:52:54 -0300

    Script final para pasar los datos humanos a JSON

--- Datasets/VisualSearchZeroShot/save_to_json.py
+++ Datasets/VisualSearchZeroShot/save_to_json.py
@@ -18,6 +18,8 @@ targetsPropertiesFile = open('../../Models/VisualSearchZeroShot/targets_position
 targetsPropertiesData = json.load(targetsPropertiesFile)
 
 numImages = 240
+window_size = (200, 200)
+image_size = (1024, 1280)
 
 trialsSequence = loadmat(trialsSequenceFile)
 trialsSequence = trialsSequence['seq'].flatten()
@@ -34,22 +36,23 @@ for subjectDataFile in subjectsFiles:
     subjectNumber = subjectDataFile[4:6]
     print("Processing " + subjectsFilesDir + subjectDataFile)
     currentSubjectData = loadmat(subjectsFilesDir + subjectDataFile)
-    stimuliProcessed = np.empty(numImages * 17)
+    stimuliProcessed = []
     for trialNumber in range(len(trialsSequence)):
         stimuli = trialsSequence[trialNumber]
         if (stimuli == 0): stimuli = 240
         if (stimuli in stimuliProcessed):
             continue
         
-        # Get fixation coordinates for the current trial
-        fix_posX = currentSubjectData['FixData']['Fix_posx'][0][0][trialNumber][0].flatten()
-        fix_posY = currentSubjectData['FixData']['Fix_posy'][0][0][trialNumber][0].flatten()
+        # Get fixation coordinates for the current trial; minus one, since Python indexes images from zero.
+        fix_posX = currentSubjectData['FixData']['Fix_posx'][0][0][trialNumber][0].flatten() - 1
+        fix_posY = currentSubjectData['FixData']['Fix_posy'][0][0][trialNumber][0].flatten() - 1
         # First fixation is always at the center of the stimuli, it isn't taken into account in the number of fixations
         number_of_fixations = len(fix_posX) - 1
         target_found = False
         # TargetFound matrix only has 74 columns
         if (number_of_fixations < 74):
             target_found = bool(currentSubjectData['FixData']['TargetFound'][0][0][trialNumber][(number_of_fixations - 1)])
+            target_found = target_found or bool(currentSubjectData['FixData']['TargetFound'][0][0][trialNumber][(number_of_fixations)])
 
         # Get target position information
         current_target = targetsPropertiesData[stimuli - 1]
@@ -57,21 +60,29 @@ for subjectDataFile in subjectsFiles:
         target_start_column = current_target['matched_column']
         target_end_row = target_start_row + current_target['target_side_length']
         target_end_column = target_start_column + current_target['target_columns']
-        # Image size was different, rescale positions
-        rescaled_tg_start_row, rescaled_tg_start_column, rescaled_tg_end_row, rescaled_tg_end_column = \
-             rescaleCoordinates(target_start_row, target_start_column, target_end_row, target_end_column, current_target['image_height'], current_target['image_width'], 1028, 1280) 
-            
-        target_bounding_box = [rescaled_tg_start_row, rescaled_tg_start_column, rescaled_tg_end_row, rescaled_tg_end_column]
 
-        last_fixation_X = fix_posX[len(fix_posX) - 1]
-        last_fixation_Y = fix_posY[len(fix_posY) - 1]
-        between_bounds = ((rescaled_tg_start_row - 10 < last_fixation_Y) and (rescaled_tg_end_row + 10 > last_fixation_Y)) and ((rescaled_tg_start_column - 10 < last_fixation_X) and (rescaled_tg_end_column + 10 > last_fixation_X))
+        target_bounding_box = [target_start_row, target_start_column, target_end_row, target_end_column]
+
+        target_center = (target_start_row + (current_target['target_side_length'] // 2), target_start_column + (current_target['target_columns'] // 2))
+        target_lower_row_bound  = target_center[0] - window_size[0]
+        target_upper_row_bound  = target_center[0] + window_size[0]
+        target_lower_column_bound = target_center[1] - window_size[1]
+        target_upper_column_bound = target_center[1] + window_size[1]
+        if (target_lower_row_bound < 0): target_lower_row_bound = 0
+        if (target_upper_row_bound > image_size[0]): target_upper_row_bound = image_size[0]
+        if (target_lower_column_bound < 0): target_lower_column_bound = 0
+        if (target_upper_column_bound > image_size[1]): target_upper_column_bound = image_size[1]
+
+        last_fixation_X = fix_posX[number_of_fixations]
+        last_fixation_Y = fix_posY[number_of_fixations]
+        between_bounds = (target_lower_row_bound <= last_fixation_Y) and (target_upper_row_bound >= last_fixation_Y) and (target_lower_column_bound <= last_fixation_X) and (target_upper_column_bound >= last_fixation_X)
         if (target_found):
-            targets_found += 1
-        if (target_found and not(between_bounds)):
-            print("Subject: " + subjectNumber + ", stimuli: " + str(stimuli) + ". Last fixation doesn't match target bounds")
-            wrong_targets_found += 1
-
+            if (between_bounds):
+                targets_found += 1
+            else:
+                print("Subject: " + subjectNumber + "; stimuli: " + str(stimuli) + "; trial: " + str(trialNumber) + ". Last fixation doesn't match target's bounds")
+                print("Target's bounds: " + str((target_lower_row_bound, target_lower_column_bound, target_upper_row_bound, target_upper_column_bound)) + ". Last fixation: " + str((last_fixation_Y, last_fixation_X)))
+                wrong_targets_found += 1
 
         fix_startTime = currentSubjectData['FixData']['Fix_starttime'][0][0][trialNumber][0].flatten()
         fix_endTime = currentSubjectData['FixData']['Fix_time'][0][0][trialNumber][0].flatten()
@@ -84,9 +95,9 @@ for subjectDataFile in subjectsFiles:
             stimuliName = '0' + stimuliName
         imageName = 'img' + str(stimuliName) + '.jpg'
 
-        subjectsTrialsInfo.append({ "subject" : subjectNumber, "image" : imageName, "dataset" : "VisualSearchZeroShot Natural Design Dataset", "image_height" : 1028, "image_width" : 1280, "target_found" : str(target_found), \
-            "target_bbox" : target_bounding_box, "X" : fix_posX.tolist(), "Y" : fix_posY.tolist(), "T" : fix_time.tolist(), "split" : "valid", "target_object" : "te la debo", "max_fixations" : "80"})
-        np.append(stimuliProcessed, stimuli)
+        subjectsTrialsInfo.append({ "subject" : subjectNumber, "image" : imageName, "dataset" : "VisualSearchZeroShot Natural Design Dataset", "image_height" : image_size[0], "image_width" : image_size[1], "window_height" : window_size[0], "window_width" : window_size[1], \
+            "target_found" : str(target_found), "target_bbox" : target_bounding_box, "X" : fix_posX.tolist(), "Y" : fix_posY.tolist(), "T" : fix_time.tolist(), "split" : "valid", "target_object" : "te la debo", "max_fixations" : "80"})
+        stimuliProcessed.append(stimuli)
 
 print("Targets found: " + str(targets_found) + ". Wrong targets found: " + str(wrong_targets_found))
 jsonStructsFile = open(subjectsFilesDir + 'human_scanpaths.json', 'w')
