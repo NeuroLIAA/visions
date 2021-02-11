c71ed54780e4a7d819a36973d07238f7740b2939
FerminT <fermintravi@gmail.com>
2021-02-11 16:43:13 -0300

    Script para convertir stimuli y target a gris

---
+++ Models/VisualSearchZeroShot/convert_to_grayscale.py
@@ -0,0 +1,24 @@
+from os import listdir
+from skimage import io, color
+
+stimuliDir = './stimuli/'
+targetDir = './target/'
+
+stimuliFiles = sorted(listdir(stimuliDir))
+targetFiles = sorted(listdir(targetDir))
+
+for stimuliFile in stimuliFiles:
+    if not(stimuliFile.endswith('.jpg')):
+        continue
+
+    stimuliImg = io.imread(stimuliDir + stimuliFile)
+    stimuliImg = color.rgb2gray(stimuliImg)
+    io.imsave(stimuliDir + stimuliFile, stimuliImg)
+
+for targetFile in targetFiles:
+    if not(targetFile.endswith('.jpg')):
+        continue
+
+    targetImg = io.imread(targetDir + targetFile)
+    targetImg = color.rgb2gray(targetImg)
+    io.imsave(targetDir + targetFile, targetImg)
