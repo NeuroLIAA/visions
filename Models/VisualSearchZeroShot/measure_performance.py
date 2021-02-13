39c6c9d158e6712b927c6a9b0d40adb5b54b8b65
FerminT <fermintravi@gmail.com>
2021-02-13 17:03:25 -0300

    Más legibilidad en la comparación de modelos

--- Models/VisualSearchZeroShot/measure_performance.py
+++ Models/VisualSearchZeroShot/measure_performance.py
@@ -5,7 +5,7 @@ import math
 import matplotlib.pyplot as plt
 
 resultsDir = 'results/'
-maxScanpathLength = 20
+maxScanpathLength = 30
 totalStimuli = 240
 
 def main():
@@ -20,20 +20,22 @@ def main():
         jsonFile.close()
         fixationsUntilTargetFound = []
 
-        for index in range(0,maxScanpathLength): # los índices irían en el eje X y los valores del índice correspondiente en el eje Y
+        for index in range(0, maxScanpathLength): # los índices irían en el eje X y los valores del índice correspondiente en el eje Y
             fixationsUntilTargetFound.append(0) 
         for struct in jsonStructs:
             if (type(struct['X']) is not list): # Hay algunos scanpaths que en matlab se guardaron como int en lugar de una lista con un solo int
-                struct['X'] = [struct['X']]
-                #struct['Y'] = [struct['Y']]
-                    
+                struct['X'] = [struct['X']]                    
 
-            if (len(struct['X'])  < maxScanpathLength + 1 ): #tendría que fijarme que el campo "target found" esté en true pero en matlab no estaba ese campo y además en todos los casos encuentra el target (creo)
-                for index in range(len(struct['X'])-1,maxScanpathLength): #el gráfico tiene que ser acumulativo, si se encontró con 2 fijaciones, también con 3,4,etc.
-                    fixationsUntilTargetFound[index] +=1
+            if (len(struct['X']) < maxScanpathLength + 1): #tendría que fijarme que el campo "target found" esté en true pero en matlab no estaba ese campo y además en todos los casos encuentra el target (creo)
+                for index in range(len(struct['X']) - 1, maxScanpathLength): #el gráfico tiene que ser acumulativo, si se encontró con 2 fijaciones, también con 3,4,etc.
+                    fixationsUntilTargetFound[index] += 1
         
-            cumulativePerformance = list(map(lambda x: float(x)/240.0,fixationsUntilTargetFound ))
-        ax.plot(range(1,maxScanpathLength+1),cumulativePerformance,label = jsonFileName)
+            cumulativePerformance = list(map(lambda x: float(x) / 240.0, fixationsUntilTargetFound))
+        label = jsonFileName[:-5].split('_')[1]
+        ax.plot(range(1, maxScanpathLength + 1), cumulativePerformance, label = label)
     ax.legend()   
+    plt.xlabel('Number of fixations')
+    plt.ylabel('Cummulative performance')
     plt.show()
+
 main()
