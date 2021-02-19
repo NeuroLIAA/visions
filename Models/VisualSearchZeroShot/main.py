79ff10cd3d58c9cbac2944d0cc5b09e04303b420
Gonzalo <gonzalorpg@gmail.com>
2021-02-19 20:08:57 -0300

    cambiado el id en los scripts

--- Models/VisualSearchZeroShot/main.py
+++ Models/VisualSearchZeroShot/main.py
@@ -1,6 +1,7 @@
 import IVSN
 import image_preprocessing
 import compute_scanpaths
+import json
 
 stimuli_dir = 'stimuli/'
 target_dir = 'target/'
@@ -11,11 +12,14 @@ max_fixations  = 80
 receptive_size = 200
 
 def main():
+    targetsLocationsFile = open('targets_locations.json')
+    targetsLocations = json.load(targetsLocationsFile)
+    targetsLocationsFile.close()
     print('Preprocessing images...')
-    image_preprocessing.chop_stimuli(stimuli_dir, chopped_dir, stimuli_size)
+    image_preprocessing.chop_stimuli(stimuli_dir, chopped_dir, stimuli_size,targetsLocations)
     print('Running model...')
-    IVSN.run(stimuli_dir, target_dir, chopped_dir)
+    IVSN.run(stimuli_dir, target_dir, chopped_dir, targetsLocations)
     print('Computing scanpaths...')
-    compute_scanpaths.parse_model_data(stimuli_dir, chopped_dir, stimuli_size, max_fixations, receptive_size, save_path)
+    compute_scanpaths.parse_model_data(stimuli_dir, chopped_dir, stimuli_size, max_fixations, receptive_size, save_path, targetsLocations)
 
 main()
