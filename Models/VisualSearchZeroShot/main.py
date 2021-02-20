46fdeec2b7d73f24599866add7ee2ee9f66c5c35
FerminT <fermintravi@gmail.com>
2021-02-20 12:51:49 -0300

    Fix en compute_scanpaths.py y reordenamiento de datasets

--- Models/VisualSearchZeroShot/main.py
+++ Models/VisualSearchZeroShot/main.py
@@ -3,8 +3,9 @@ import image_preprocessing
 import compute_scanpaths
 import json
 
-stimuli_dir = 'stimuli/'
-target_dir = 'target/'
+stimuli_dir = '../../Datasets/VisualSearchZeroShot/stimuli/'
+target_dir = '../../Datasets/VisualSearchZeroShot/target/'
+targets_locations_file = 'targets_locations.json'
 chopped_dir = 'choppednaturaldesign/'
 save_path = 'results/Scanpaths.json'
 stimuli_size = (1028, 1280)
@@ -12,14 +13,14 @@ max_fixations  = 80
 receptive_size = 200
 
 def main():
-    targetsLocationsFile = open('targets_locations.json')
-    targetsLocations = json.load(targetsLocationsFile)
-    targetsLocationsFile.close()
+    with open(targets_locations_file) as fp:
+        targets_locations = json.load(fp)
+
     print('Preprocessing images...')
-    image_preprocessing.chop_stimuli(stimuli_dir, chopped_dir, stimuli_size,targetsLocations)
+    image_preprocessing.chop_stimuli(stimuli_dir, chopped_dir, stimuli_size, targets_locations)
     print('Running model...')
-    IVSN.run(stimuli_dir, target_dir, chopped_dir, targetsLocations)
+    IVSN.run(stimuli_dir, target_dir, chopped_dir, targets_locations)
     print('Computing scanpaths...')
-    compute_scanpaths.parse_model_data(stimuli_dir, chopped_dir, stimuli_size, max_fixations, receptive_size, save_path, targetsLocations)
+    compute_scanpaths.parse_model_data(stimuli_dir, chopped_dir, stimuli_size, max_fixations, receptive_size, save_path, targets_locations)
 
 main()
