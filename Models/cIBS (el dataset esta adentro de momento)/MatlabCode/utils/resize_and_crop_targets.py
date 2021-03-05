4ae044abb2e7c56bf48e365d9404be27dba918a5
Gonzalo <gonzalorpg@gmail.com>
2021-03-05 17:19:00 -0300

    agregado el crop

--- Models/cIBS (el dataset esta adentro de momento)/MatlabCode/utils/resize_and_crop_targets.py
+++ Models/cIBS (el dataset esta adentro de momento)/MatlabCode/utils/resize_and_crop_targets.py
@@ -1,29 +1,46 @@
 from skimage import io, transform, img_as_ubyte
 from os import listdir, path, mkdir, rename
+import json
 
 datasetDir = '../data_images/images/'
 targetsDir = '../data_images/templates/'
 datasetDirOld = '../data_images/images_old/'
 targetsDirOld = '../data_images/templates_old/'
-stimuli_size = (1028, 1280)
+stimuli_size = (768, 1024)
+trials_properties_file = '../data_images/trials_properties_IVSN.json'
 
 def main():
         
     if not(path.exists(datasetDirOld)):
         mkdir(datasetDirOld)
-    if not(path.exists(targetsDir)):
-        mkdir(targetsDir)
+    if not(path.exists(targetsDirOld)):
+        mkdir(targetsDirOld)
+    with open(trials_properties_file) as fp:
+        trials_properties = json.load(fp)
+    
     for template_file in listdir(targetsDir):
         if not(template_file.endswith('.jpg')):
             continue
-        rename(targetsDir + '/' + template_file, targetsDirOld + '/' + template_file)
-    for image_file in listdir(datasetDir):
-        if not(image_file.endswith('.jpg')):
-            continue
-        rename(datasetDir + '/' + image_file, datasetDirOld + '/' + image_file)
-        image_resized = io.imread(datasetDirOld + '/' + image_file)
+        rename(targetsDir + template_file, targetsDirOld + template_file)
+
+    for trial_properties in trials_properties:
+        image_file = trial_properties['image']
+        rename(datasetDir + image_file, datasetDirOld + image_file)
+        image_resized = io.imread(datasetDirOld + image_file)
         image_resized = transform.resize(image_resized, stimuli_size)
-        io.imsave(datasetDir + '/' + image_file, img_as_ubyte(image_resized), check_contrast=False)
-        #falta el crop
-        
+        io.imsave(datasetDir + image_file, img_as_ubyte(image_resized), check_contrast=False)
+        target_bbox = (trial_properties['target_matched_row'], trial_properties['target_matched_column'], trial_properties['target_side_length'] + trial_properties['target_matched_row'], \
+        trial_properties['target_columns'] + trial_properties['target_matched_column'])
+        target_bbox = rescale_coordinates(target_bbox[0], target_bbox[1], target_bbox[2], target_bbox[3], trial_properties['image_height'], trial_properties['image_width'], stimuli_size[0], stimuli_size[1])
+        template = image_resized[target_bbox[0]:target_bbox[2], target_bbox[1]:target_bbox[3]]       
+        io.imsave(targetsDir + image_file[:-4] + '_template.jpg', img_as_ubyte(template), check_contrast=False)
+
+def rescale_coordinates(start_row, start_column, end_row, end_column, img_height, img_width, new_img_height, new_img_width):
+    rescaled_start_row = round((start_row / img_height) * new_img_height)
+    rescaled_start_column = round((start_column / img_width) * new_img_width)
+    rescaled_end_row = round((end_row / img_height) * new_img_height)
+    rescaled_end_column = round((end_column / img_width) * new_img_width)
+
+    return rescaled_start_row, rescaled_start_column, rescaled_end_row, rescaled_end_column
+    
 main()
