649472be065990909baa45f54a38a7b5586fb134
FerminT <fermintravi@gmail.com>
2021-04-13 10:58:13 -0300

    Casteos necesarios

--- Models/cIBS (el dataset esta adentro de momento)/MatlabCode/utils/resize_and_crop_targets.py
+++ Models/cIBS (el dataset esta adentro de momento)/MatlabCode/utils/resize_and_crop_targets.py
@@ -22,16 +22,16 @@ def main():
     with open(trials_properties_file) as fp:
         trials_properties = json.load(fp)
     
-    for template_file in listdir(targetsDir):
-        if not(template_file.endswith('.jpg')):
-            continue
-        rename(targetsDir + template_file, targetsDirOld + template_file)
+    # for template_file in listdir(targetsDir):
+    #     if not(template_file.endswith('.jpg')):
+    #         continue
+    #     rename(targetsDir + template_file, targetsDirOld + template_file)
 
     for trial_properties in trials_properties:
         image_file = trial_properties['image']
         rename(datasetDir + image_file, datasetDirOld + image_file)
         image_resized = io.imread(datasetDirOld + image_file)
-        image_resized = color.rgb2gray(image_resized)
+        #image_resized = color.rgb2gray(image_resized)
         image_resized = transform.resize(image_resized, stimuli_size)
         io.imsave(datasetDir + image_file, img_as_ubyte(image_resized), check_contrast=False)
         target_bbox = (trial_properties['target_matched_row'], trial_properties['target_matched_column'], trial_properties['target_side_length'] + trial_properties['target_matched_row'], \
@@ -40,7 +40,7 @@ def main():
         template = image_resized[target_bbox[0]:target_bbox[2], target_bbox[1]:target_bbox[3]]       
         io.imsave(targetsDir + image_file[:-4] + '_template.jpg', img_as_ubyte(template), check_contrast=False)
         target_positions["target_positions"].append({ "image" : image_file, "template" : image_file[:-4] + '_template.jpg', "matched_column" : target_bbox[1] + 1, "matched_row" : target_bbox[0] + 1, "template_side_length" : target_bbox[2] - target_bbox[0], "template_columns" : target_bbox[3] - target_bbox[1] }) #side length y columns son distancias, no hace falta sumar 1
-        initial_fixations["initial_fixations"].append({"image" : image_file, "initial_fix" : (trial_properties['initial_fixation_x'],trial_properties['initial_fixation_y'])})
+        initial_fixations["initial_fixations"].append({"image" : image_file, "initial_fix" : (trial_properties['initial_fixation_x'] + 1, trial_properties['initial_fixation_y'] + 1)})
     savemat("target_positions_filtered.mat", target_positions)
     savemat("initial_fixations.mat", initial_fixations)
 
