49e9415186cbc21267632ae882d264b088e0d159
Gonzalo <gonzalorpg@gmail.com>
2021-05-15 13:49:30 -0300

    fix bbox en .json

--- Models/cIBS (el dataset esta adentro de momento)/MatlabCode/utils/mat_to_json_scanpaths_data.py
+++ Models/cIBS (el dataset esta adentro de momento)/MatlabCode/utils/mat_to_json_scanpaths_data.py
@@ -1,3 +1,4 @@
+from numpy.core.defchararray import less_equal
 from scipy.io import loadmat
 import json
 import os
@@ -43,20 +44,26 @@ for cfg_file in cfg_files:
 
     delta = int(cfg_info['delta'][0][0][0][0])
 
-    target_center_top_left = cfg_info['target_center_top_left'][0][0][0]
-    target_center_bot_right = cfg_info['target_center_bot_right'][0][0][0]
-    target_size = cfg_info['target_size'][0][0][0]
+    target_cell_top_left = np.array(cfg_info['target_center_top_left'][0][0][0])
+    target_cell_bot_right = np.array(cfg_info['target_center_bot_right'][0][0][0])
+    target_size = np.array(cfg_info['target_size'][0][0][0])
+    target_center = np.array(cfg_info['target_center'][0][0][0])
 
-    target_center_top_left_pixels  = mapCellToFixation(target_center_top_left.astype(int), image_height, image_width, delta)
-    target_center_bot_right_pixels = mapCellToFixation(target_center_bot_right.astype(int), image_height, image_width, delta)
-    target_bbox = [int(target_center_top_left_pixels[0]), int(target_center_top_left_pixels[1]), int(target_center_bot_right_pixels[0]), int(target_center_bot_right_pixels[1])]
+    target_top_left = target_center - target_size/2
+    target_bot_right = target_center + target_size/2
+    
+    
+    target_bbox = [int(target_top_left[0]), int(target_top_left[1]), int(target_bot_right[0]), int(target_bot_right[1])]
 
+    number_of_fixations = len(scanpath)
+    last_fixation = np.array(scanpath[number_of_fixations - 1])
+    
+    between_bounds = (target_cell_top_left <= last_fixation) & (target_cell_bot_right <= last_fixation)
+    
     number_of_fixations = len(scanpath)
     last_fixation = scanpath[number_of_fixations - 1]
 
-    between_bounds = (target_center_top_left[0] <= last_fixation[0]) and (target_center_bot_right[0] >= last_fixation[0]) and (target_center_top_left[1] <= last_fixation[1]) and (target_center_bot_right[1] >= last_fixation[1])
-
-    if (number_of_fixations < 31) or between_bounds:
+    if (number_of_fixations < 31) and np.all(between_bounds):
         target_found = True
     else:
         target_found = False
