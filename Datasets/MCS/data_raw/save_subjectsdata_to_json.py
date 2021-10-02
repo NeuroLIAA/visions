0b9c46aeda8d97449ce0f418922fd811cb270168
FerminT <fermintravi@gmail.com>
2021-10-02 11:45:38 -0300

    (MCS) Scripts para criterio delta

--- Datasets/MCS/data_raw/save_subjectsdata_to_json.py
+++ Datasets/MCS/data_raw/save_subjectsdata_to_json.py
@@ -1,6 +1,6 @@
 import json
 from os import listdir, path, mkdir
-from utils import rename_image, rescale_coordinate, convert_coordinate
+import utils
 import pandas as pd
 import numpy as np
 
@@ -9,16 +9,15 @@ train_clock_scanpaths_file     = 'gaze/train/clock_fixations.csv'
 test_scanpaths_file            = 'gaze/test/clock_and_microwave_fixations.csv'
 
 targets_bboxes_file = 'targets_bboxes.json'
-with open(targets_bboxes_file, 'r') as fp:
-    targets_bboxes = json.load(fp)
+targets_bboxes      = utils.load_dict_from_json(targets_bboxes_file)
 
 display_size_train = (800, 1280)
 display_size_test  = (1050, 1680)
 new_image_size     = (508, 564)
 
 # Estimated by measuring the distance between consecutive fixations
-receptive_height = 25
-receptive_width  = 25
+receptive_height = 20
+receptive_width  = 20
 # Unlimited
 max_fixations = 80
 
@@ -30,7 +29,6 @@ test_scanpaths            = pd.read_csv(test_scanpaths_file)
 train_scanpaths           = pd.concat([train_microwave_scanpaths, train_clock_scanpaths])
 
 subjects_scanpaths = {}
-number_of_targets_found = 0
 number_of_trials = 0
 
 last_trial_processed = 0
@@ -40,6 +38,7 @@ trial_scanpath = {}
 rescaled_target_bbox = []
 image_category = ''
 target_found = False
+unanotated   = False
 # Process train scanpaths
 for index, row in train_scanpaths.iterrows():
     if row['condition'] == 'absent':
@@ -52,10 +51,8 @@ for index, row in train_scanpaths.iterrows():
 
     if last_trial_processed != trial_index:
         if trial_scanpath:
-            image_name = rename_image(image_name, image_category)
+            image_name = utils.rename_image(image_name, image_category)
             number_of_trials += 1
-            if target_found:
-                number_of_targets_found += 1
             if subject_num in subjects_scanpaths:
                 subjects_scanpaths[subject_num][image_name] = trial_scanpath
             else:
@@ -73,39 +70,39 @@ for index, row in train_scanpaths.iterrows():
         if not image_name in targets_bboxes[category]:
             target_found = True
             continue
+
         img_info = targets_bboxes[category][image_name]
         original_img_size = (img_info['image_height'], img_info['image_width'])
         target_bbox = img_info['target_bbox']
-        rescaled_target_bbox = [int(rescale_coordinate(target_bbox[i], original_img_size[i % 2 == 1], new_image_size[i % 2 == 1])) for i in range(len(target_bbox))]
+        rescaled_target_bbox = [int(utils.rescale_coordinate(target_bbox[i], original_img_size[i % 2 == 1], new_image_size[i % 2 == 1])) for i in range(len(target_bbox))]
         # Edge cases
         rescaled_target_bbox[2] = min(new_image_size[0] - 1, rescaled_target_bbox[2])
         rescaled_target_bbox[3] = min(new_image_size[1] - 1, rescaled_target_bbox[3])
 
         if subject_num < 10:
             subject_name = '0' + str(subject_num)
+
         trial_scanpath = {'subject': subject_name, 'dataset': 'MCS Dataset', 'image_height': new_image_size[0], 'image_width': new_image_size[1], \
-            'screen_height': display_size_train[0], 'screen_width': display_size_train[1],  'receptive_height': 10, 'receptive_width': 10, 'target_found': False, \
+            'screen_height': display_size_train[0], 'screen_width': display_size_train[1],  'receptive_height': receptive_height, 'receptive_width': receptive_width, 'target_found': False, \
                 'target_bbox': rescaled_target_bbox, 'X': [], 'Y': [], 'T': [], 'target_object': category, 'max_fixations': max_fixations}
 
-    current_fix_x, current_fix_y = convert_coordinate(row['CURRENT_FIX_X'], row['CURRENT_FIX_Y'], new_image_size[1], new_image_size[0], display_size_train, is_train=True)
+    current_fix_x, current_fix_y = utils.convert_coordinate(row['CURRENT_FIX_X'], row['CURRENT_FIX_Y'], new_image_size[1], new_image_size[0], display_size_train, is_train=True)
 
     trial_scanpath['X'].append(current_fix_x)
     trial_scanpath['Y'].append(current_fix_y)
     trial_scanpath['T'].append(row['CURRENT_FIX_DURATION'])
 
-    target_found = (rescaled_target_bbox[0] <= current_fix_y + receptive_height) and (rescaled_target_bbox[2] >= current_fix_y - receptive_height) and \
-                    (rescaled_target_bbox[1] <= current_fix_x + receptive_width) and (rescaled_target_bbox[3] >= current_fix_x - receptive_width)
+    target_found = utils.between_bounds(rescaled_target_bbox, current_fix_y, current_fix_x, (receptive_height, receptive_width))
 
     trial_scanpath['target_found'] = target_found
 
 # Save last record
 if trial_scanpath:
-    image_name = rename_image(image_name, image_category)
+    image_name = utils.rename_image(image_name, image_category)
     if subject_num in subjects_scanpaths:
         subjects_scanpaths[subject_num][image_name] = trial_scanpath
     else:
         subjects_scanpaths[subject_num] = {image_name : trial_scanpath}
-
 
 last_trial_processed = 0
 subject_num = 0
@@ -126,10 +123,8 @@ for index, row in test_scanpaths.iterrows():
 
     if last_trial_processed != trial_index:
         if trial_scanpath:
-            image_name = rename_image(image_name, image_category)
+            image_name = utils.rename_image(image_name, image_category)
             number_of_trials += 1
-            if target_found:
-                number_of_targets_found += 1
             if subject_num in subjects_scanpaths:
                 subjects_scanpaths[subject_num][image_name] = trial_scanpath
             else:
@@ -153,7 +148,7 @@ for index, row in test_scanpaths.iterrows():
         img_info = targets_bboxes[category][image_name]
         original_img_size = (img_info['image_height'], img_info['image_width'])
         target_bbox = img_info['target_bbox']
-        rescaled_target_bbox = [int(rescale_coordinate(target_bbox[i], original_img_size[i % 2 == 1], new_image_size[i % 2 == 1])) for i in range(len(target_bbox))]
+        rescaled_target_bbox = [int(utils.rescale_coordinate(target_bbox[i], original_img_size[i % 2 == 1], new_image_size[i % 2 == 1])) for i in range(len(target_bbox))]
 
         if subject_num < 10:
             subject_name = '0' + str(subject_num)
@@ -163,20 +158,19 @@ for index, row in test_scanpaths.iterrows():
             'screen_height': display_size_train[0], 'screen_width': display_size_train[1],  'receptive_height': 10, 'receptive_width': 10, 'target_found': False, \
                 'target_bbox': rescaled_target_bbox, 'X': [], 'Y': [], 'T': [], 'target_object': category, 'max_fixations': max_fixations}
 
-    current_fix_x, current_fix_y = convert_coordinate(row['CURRENT_FIX_X'], row['CURRENT_FIX_Y'], new_image_size[1], new_image_size[0], display_size_test, is_train=False)
+    current_fix_x, current_fix_y = utils.convert_coordinate(row['CURRENT_FIX_X'], row['CURRENT_FIX_Y'], new_image_size[1], new_image_size[0], display_size_test, is_train=False)
 
     trial_scanpath['X'].append(current_fix_x)
     trial_scanpath['Y'].append(current_fix_y)
     trial_scanpath['T'].append(row['CURRENT_FIX_DURATION'])
 
-    target_found = (rescaled_target_bbox[0] <= current_fix_y + receptive_height) and (rescaled_target_bbox[2] >= current_fix_y - receptive_height) and \
-                    (rescaled_target_bbox[1] <= current_fix_x + receptive_width) and (rescaled_target_bbox[3] >= current_fix_x - receptive_width)
+    target_found = utils.between_bounds(rescaled_target_bbox, current_fix_y, current_fix_x, (receptive_height, receptive_width))
 
     trial_scanpath['target_found'] = target_found
 
 # Save last record
 if trial_scanpath:
-    image_name = rename_image(image_name, image_category)
+    image_name = utils.rename_image(image_name, image_category)
     if subject_num in subjects_scanpaths:
         subjects_scanpaths[subject_num][image_name] = trial_scanpath
     else:
@@ -185,9 +179,10 @@ if trial_scanpath:
 if not path.exists(save_path):
     mkdir(save_path)
 
-scanpaths_with_shorter_distance_than_receptive_size = 0
-
-# Save subjects scanpaths as JSON files and estimate the distance between fixations
+# Save subjects scanpaths as JSON files and collapse fixations
+collapsed_scanpaths = 0
+collapsed_fixations = 0
+targets_found       = 0
 for subject in subjects_scanpaths:
     if subject < 10:
         subject_name = '0' + str(subject)
@@ -196,21 +191,19 @@ for subject in subjects_scanpaths:
     subject_scanpaths_file = 'subj' + subject_name + '_scanpaths.json'
 
     subject_scanpaths = subjects_scanpaths[subject]
+
     for trial in subject_scanpaths:
         scanpath = subject_scanpaths[trial]
-        # Check for distance between consecutive fixations
-        if scanpath['target_found'] and len(scanpath['X']) > 1:
-            fixations = [fix for fix in zip(scanpath['X'], scanpath['Y'])]
-            distance_between_consecutive_fixations          = [np.linalg.norm(np.array(fix_1) - np.array(fix_2)) for fix_1, fix_2 in zip(fixations, fixations[1:])]
-            shortest_distance_between_consecutive_fixations = min(distance_between_consecutive_fixations)
+        original_scanpath_length = len(scanpath['X'])
+        targets_found += scanpath['target_found']
+        # # Collapse consecutive fixations which are closer than receptive_size / 2
+        # scanpath['X'], scanpath['Y'] = utils.collapse_fixations(scanpath['X'], scanpath['Y'], (receptive_height, receptive_width))
+        # if len(scanpath['X']) < original_scanpath_length:
+        #     collapsed_scanpaths += 1
+        #     collapsed_fixations += original_scanpath_length - len(scanpath['X'])
 
-            fixation_number = distance_between_consecutive_fixations.index(shortest_distance_between_consecutive_fixations)
-            shortest_consecutive_fixations_distance = (abs(scanpath['X'][fixation_number] - scanpath['X'][fixation_number + 1]), abs(scanpath['Y'][fixation_number] - scanpath['Y'][fixation_number + 1]))
-            if shortest_consecutive_fixations_distance[0] < receptive_height // 2 and shortest_consecutive_fixations_distance[1] < receptive_width // 2:
-                scanpaths_with_shorter_distance_than_receptive_size += 1
+    utils.save_to_json(path.join(save_path, subject_scanpaths_file), subjects_scanpaths[subject])
 
-    with open(save_path + subject_scanpaths_file, 'w') as fp:
-        json.dump(subjects_scanpaths[subject], fp, indent=4)
-
-print('Total targets found: ' + str(number_of_targets_found) + '/' + str(number_of_trials) + ' trials')
-print('Scanpaths where saccades have shorter distance than ' + str((receptive_height, receptive_width)) + ': ' + str(scanpaths_with_shorter_distance_than_receptive_size))
+print('Total targets found: ' + str(targets_found) + '/' + str(number_of_trials) + ' trials')
+print("Collapsed scanpaths (discretized in size " + str((receptive_height, receptive_width)) + ") : " + str(collapsed_scanpaths))
+print("Number of fixations collapsed: " + str(collapsed_fixations))
--- Datasets/MCS/data_raw/utils.py
+++ Datasets/MCS/data_raw/utils.py
@@ -1,3 +1,52 @@
+import json
+
+def load_dict_from_json(json_file):
+    with open(json_file, 'r') as fp:
+        return json.load(fp)
+
+def save_to_json(json_file, data):
+    with open(json_file, 'w') as fp:
+        json.dump(data, fp, indent=4)
+
+def between_bounds(target_bbox, fix_y, fix_x, receptive_size):
+    return target_bbox[0] <= fix_y + receptive_size[0] // 2 and target_bbox[2] >= fix_y - receptive_size[0] // 2 and \
+            target_bbox[1] <= fix_x + receptive_size[1] // 2 and target_bbox[3] >= fix_x - receptive_size[1] // 2
+
+def crop_scanpath(scanpath_x, scanpath_y, target_bbox, receptive_size):
+    target_found = False
+    cropped      = False
+    original_len = len(scanpath_x)
+    for index, fixation in enumerate(zip(scanpath_y, scanpath_x)):
+        if between_bounds(target_bbox, fixation[0], fixation[1], receptive_size):
+            scanpath_x = scanpath_x[:index + 1]
+            scanpath_y = scanpath_y[:index + 1]
+            target_found = True
+            break
+    
+    if len(scanpath_x) < original_len: cropped = True
+    
+    return target_found, cropped
+
+def collapse_fixations(scanpath_x, scanpath_y, receptive_size):
+    collapsed_scanpath_x = list(scanpath_x)
+    collapsed_scanpath_y = list(scanpath_y)
+    index = 0
+    while index < len(collapsed_scanpath_x) - 1:
+        abs_difference_x = [abs(fix_1 - fix_2) for fix_1, fix_2 in zip(collapsed_scanpath_x, collapsed_scanpath_x[1:])]
+        abs_difference_y = [abs(fix_1 - fix_2) for fix_1, fix_2 in zip(collapsed_scanpath_y, collapsed_scanpath_y[1:])]
+
+        if abs_difference_x[index] < receptive_size[1] / 2 and abs_difference_y[index] < receptive_size[0] / 2:
+            new_fix_x = (collapsed_scanpath_x[index] + collapsed_scanpath_x[index + 1]) / 2
+            new_fix_y = (collapsed_scanpath_y[index] + collapsed_scanpath_y[index + 1]) / 2
+            collapsed_scanpath_x[index] = new_fix_x
+            collapsed_scanpath_y[index] = new_fix_y
+            del collapsed_scanpath_x[index + 1]
+            del collapsed_scanpath_y[index + 1]
+        else:
+            index += 1
+
+    return collapsed_scanpath_x, collapsed_scanpath_y
+
 def convert_coordinate(X, Y, im_w, im_h, display_size, is_train):
     """
     convert from display coordinate to pixel coordinate
