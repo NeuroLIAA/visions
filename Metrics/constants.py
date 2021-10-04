27320b2fb16c4ccb2a9b05cfe94fdacd3e0df78a
FerminT <fermintravi@gmail.com>
2021-10-04 11:24:58 -0300

    (Results) Plots rename cIBS -> nnIBS

--- Metrics/constants.py
+++ Metrics/constants.py
@@ -6,5 +6,5 @@ COCOSEARCH_SUBSET_SIZE = 500
 RANDOM_SEED = 1234
 
 # To ensure models have the same color in the plots across all datasets
-MODELS_COLORS = ['#2ca02c', '#d62728', '#ff7f0e']
+MODELS_COLORS = ['#2ca02c', '#ff7f0e', '#d62728']
 HUMANS_COLOR  = '#1f77b4'
