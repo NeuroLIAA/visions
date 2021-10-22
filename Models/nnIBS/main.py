9be0aed5fd06db587ba8b3ea85035b27a27e5b06
FerminT <fermintravi@gmail.com>
2021-10-22 19:48:28 -0300

    (nnIBS) Fix para correrlo desde método main

--- Models/nnIBS/main.py
+++ Models/nnIBS/main.py
@@ -22,8 +22,8 @@ def setup_and_run(dataset_name, config_name, image_name, image_range, human_subj
     visualsearch.run(config, dataset_info, trials_properties, human_scanpaths, output_path, constants.SIGMA)
 
 """ Main method, added to be polymorphic with respect to the other models """
-def main(dataset_name, human_subject):
-    setup_and_run(dataset_name, config_name=default, image_name=None, image_range=None, human_subject=human_subject, number_of_processes='all', save_probability_maps=True)
+def main(dataset_name, human_subject=None):
+    setup_and_run(dataset_name, config_name='default', image_name=None, image_range=None, human_subject=human_subject, number_of_processes='all', save_probability_maps=False)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run the cIBS Visual Search model')
