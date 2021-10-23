764f00369baa2e1068171cde02ca75490978eb74
FerminT <fermintravi@gmail.com>
2021-10-23 13:42:51 -0300

    (MCS dataset) Quita de scanpaths triviales (largo == 1)

--- Datasets/MCS/data_raw/remove_trivial_trials.py
+++ Datasets/MCS/data_raw/remove_trivial_trials.py
@@ -25,13 +25,21 @@ for trial in list(trials_properties):
 trials_properties_file = '../trials_properties_cropped.json'
 utils.save_to_json(trials_properties_file, trials_properties)
 
+trivial_scanpaths = 0
 for subject in human_scanpaths_files:
     subject_scanpaths = utils.load_dict_from_json(path.join(human_scanpaths_dir, subject))
     for image in removed_images:
         if image in subject_scanpaths:
             del subject_scanpaths[image]
     
+    for trial in dict(subject_scanpaths):
+        scanpath = subject_scanpaths[trial]
+        if len(scanpath['X']) == 1:
+            trivial_scanpaths += 1
+            del subject_scanpaths[trial]
+    
     utils.save_to_json(path.join(human_scanpaths_dir, subject), subject_scanpaths)
 
 print('Removed images: ' + str(removed_images))
 print('Number of removed images: ' + str(len(removed_images)))
+print('Trivial scanpaths (after removing images): ' + str(trivial_scanpaths))
