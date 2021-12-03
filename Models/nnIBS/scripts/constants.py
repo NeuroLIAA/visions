696df4bbaf4a1131117ca903bdc2e7bf2441fa53
FerminT <fermintravi@gmail.com>
2021-12-03 12:23:22 -0300

    (nnIBS) Number of processes en constants

--- Models/nnIBS/scripts/constants.py
+++ Models/nnIBS/scripts/constants.py
@@ -7,5 +7,6 @@ RESULTS_PATH  = 'Results'
 SALIENCY_PATH = path.join(path.join(path.join('Models', 'nnIBS'), 'data'), 'saliency')
 TARGET_SIMILARITY_PATH = path.join(path.join(path.join('Models', 'nnIBS'), 'data'), 'target_similarity_maps')
 
+NUMBER_OF_PROCESSES = 'all'
 SIGMA      = [[4000, 0], [0, 2600]]
 IMAGE_SIZE = (768, 1024)
