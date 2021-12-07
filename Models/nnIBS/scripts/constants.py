ba447a5a313c598033d15fabaf626710bb6a4e19
FerminT <fermintravi@gmail.com>
2021-12-07 14:41:35 -0300

    (nnIBS) Refactor constants.py

--- Models/nnIBS/scripts/constants.py
+++ Models/nnIBS/scripts/constants.py
@@ -1,11 +1,12 @@
 from os import path
 # Configuration constants
 # All paths are relative to root
-CONFIG_DIR    = path.join(path.join('Models', 'nnIBS'), 'configs')
+
+CONFIG_DIR    = path.join('Models', 'nnIBS', 'configs')
 DATASETS_PATH = 'Datasets'
 RESULTS_PATH  = 'Results'
-SALIENCY_PATH = path.join(path.join(path.join('Models', 'nnIBS'), 'data'), 'saliency')
-TARGET_SIMILARITY_PATH = path.join(path.join(path.join('Models', 'nnIBS'), 'data'), 'target_similarity_maps')
+SALIENCY_PATH = path.join('Models', 'nnIBS', 'data', 'saliency')
+TARGET_SIMILARITY_PATH = path.join('Models', 'nnIBS', 'data', 'target_similarity_maps')
 
 NUMBER_OF_PROCESSES = 'all'
 SIGMA      = [[4000, 0], [0, 2600]]
