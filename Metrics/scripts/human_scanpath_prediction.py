fdc39c45c027bbb307c66f73611ed45df773d257
FerminT <fermintravi@gmail.com>
2021-12-14 15:50:15 -0300

    (Metrics) IG eliminado de hsp

--- Metrics/scripts/human_scanpath_prediction.py
+++ Metrics/scripts/human_scanpath_prediction.py
@@ -61,7 +61,7 @@ class HumanScanpathPrediction:
     
     def compute_model_mean(self, average_results_per_image, model_name):
         """ Get the average across all images for a given model in a given dataset """
-        self.models_results[model_name] = {'Human_scanpath_prediction': {'AUC': 0, 'NSS': 0, 'IG': 0}}
+        self.models_results[model_name] = {'Human_scanpath_prediction': {'AUC': 0, 'NSS': 0}}
 
         # Get subsample for COCOSearch18 dataset
         number_of_images            = min(len(average_results_per_image), self.number_of_images)
@@ -69,7 +69,6 @@ class HumanScanpathPrediction:
         for image_name in results_per_image_subsample:
             self.models_results[model_name]['Human_scanpath_prediction']['AUC'] += results_per_image_subsample[image_name]['AUC'] / number_of_images
             self.models_results[model_name]['Human_scanpath_prediction']['NSS'] += results_per_image_subsample[image_name]['NSS'] / number_of_images
-            self.models_results[model_name]['Human_scanpath_prediction']['IG']  += results_per_image_subsample[image_name]['IG'] / number_of_images
     
     def average_results(self, model_output_path):
         """ Get the average of all subjects for each image """
